c2901a349c3bb4275c6e010cbe84ba9a34f42a3f
Adrian Hesketh <adrianhesketh@hushmail.com>
2017-06-24 14:57:38 +0100

    Added filled circles and polygons.

--- composable_test.go
+++ composable_test.go
@@ -5,41 +5,55 @@ import "testing"
 func TestThatCirclesAreComposable(t *testing.T) {
 	var c interface{} = new(Circle)
 	if _, ok := c.(Composable); !ok {
-		t.Errorf("expected %t to implement Composable", c)
+		t.Error("expected Circle to implement Composable")
+	}
+}
+
+func TestThatFilledCirclesAreComposable(t *testing.T) {
+	var c interface{} = new(FilledCircle)
+	if _, ok := c.(Composable); !ok {
+		t.Error("expected Filled Circle to implement Composable")
 	}
 }
 
 func TestThatCompositionsAreComposable(t *testing.T) {
 	var c interface{} = new(Composition)
 	if _, ok := c.(Composable); !ok {
-		t.Errorf("expected %t to implement Composable", c)
+		t.Error("expected Composition to implement Composable")
 	}
 }
 
 func TestThatLinesAreComposable(t *testing.T) {
 	var c interface{} = new(Line)
 	if _, ok := c.(Composable); !ok {
-		t.Errorf("expected %t to implement Composable", c)
+		t.Error("expected Lines to implement Composable")
 	}
 }
 
 func TestThatPolygonsAreComposable(t *testing.T) {
 	var c interface{} = new(Polygon)
 	if _, ok := c.(Composable); !ok {
-		t.Errorf("expected %t to implement Composable", c)
+		t.Error("expected Polygons to implement Composable")
+	}
+}
+
+func TestThatFilledPolygonsAreComposable(t *testing.T) {
+	var c interface{} = new(FilledPolygon)
+	if _, ok := c.(Composable); !ok {
+		t.Error("expected Filled Polygons to implement Composable")
 	}
 }
 
 func TestThatSquaresAreComposable(t *testing.T) {
 	var c interface{} = new(Square)
 	if _, ok := c.(Composable); !ok {
-		t.Errorf("expected %t to implement Composable", c)
+		t.Error("expected Squares to implement Composable")
 	}
 }
 
 func TestThatTextIsComposable(t *testing.T) {
 	var c interface{} = new(Text)
 	if _, ok := c.(Composable); !ok {
-		t.Errorf("expected %t to implement Composable", c)
+		t.Error("expected Text to implement Composable")
 	}
 }
