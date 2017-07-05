848b55b6a7e807c0041ab262c27ed56c45b1dbd4
Adrian Hesketh <adrianhesketh@hushmail.com>
2017-07-05 19:04:16 +0100

    Added minimal test for Circle.

---
+++ circle_test.go
@@ -0,0 +1,53 @@
+package raster
+
+import (
+	"image"
+	"testing"
+
+	"golang.org/x/image/colornames"
+)
+
+func TestCircle(t *testing.T) {
+	c := NewCircle(image.Point{50, 50}, 50, colornames.White)
+
+	img := image.NewRGBA(image.Rect(0, 0, 101, 101))
+	c.Draw(img)
+
+	// Check the basic shape is there.
+	if img.At(50, 50) == colornames.White {
+		t.Error("expected nothing in the center")
+	}
+
+	if img.At(0, 0) == colornames.White {
+		t.Error("expected nothing at the top left")
+	}
+
+	if img.At(0, 50) != colornames.White {
+		t.Error("expected the left edge of the circle to be set")
+	}
+
+	if img.At(100, 50) != colornames.White {
+		t.Error("expected the right edge of the circle to be set")
+	}
+
+	if img.At(50, 0) != colornames.White {
+		t.Error("expected the top edge of the circle to be set")
+	}
+
+	if img.At(50, 100) != colornames.White {
+		t.Error("expected the bottom edge of the circle to be set")
+	}
+}
+
+func TestCircleBounds(t *testing.T) {
+	radius := 1000
+	c := NewCircle(image.Point{100, 100}, radius, colornames.White)
+
+	if c.Bounds().Dx() != radius*2 {
+		t.Errorf("expected 2000 diameter, but got %v", c.Bounds().Dx())
+	}
+
+	if c.Bounds().Dy() != radius*2 {
+		t.Errorf("expected 2000 diameter, but got %v", c.Bounds().Dy())
+	}
+}
