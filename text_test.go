c8193b6cc17033e8ca050211ceec7876879ef706
Adrian Hesketh <adrianhesketh@hushmail.com>
2017-07-10 18:51:40 +0100

    Added unit test for text.

--- text_test.go
+++ text_test.go
@@ -2,12 +2,77 @@ package raster
 
 import (
 	"image"
+	"image/color"
 	"testing"
 
 	"golang.org/x/image/colornames"
 )
 
 func TestText(t *testing.T) {
+	tests := []struct {
+		toWrite  string
+		expected [][]int
+	}{
+		{
+			toWrite: "H",
+			expected: [][]int{
+				[]int{0, 0, 0, 0, 0, 0, 0},
+				[]int{0, 0, 0, 0, 0, 0, 0},
+				[]int{0, 0, 0, 0, 0, 0, 0},
+				[]int{0, 0, 0, 0, 0, 0, 0},
+				[]int{1, 0, 0, 0, 0, 1, 0},
+				[]int{1, 0, 0, 0, 0, 1, 0},
+				[]int{1, 0, 0, 0, 0, 1, 0},
+				[]int{1, 0, 0, 0, 0, 1, 0},
+				[]int{1, 1, 1, 1, 1, 1, 0},
+				[]int{1, 0, 0, 0, 0, 1, 0},
+				[]int{1, 0, 0, 0, 0, 1, 0},
+				[]int{1, 0, 0, 0, 0, 1, 0},
+				[]int{1, 0, 0, 0, 0, 1, 0},
+			},
+		},
+		{
+			toWrite: "e",
+			expected: [][]int{
+				[]int{0, 0, 0, 0, 0, 0, 0},
+				[]int{0, 0, 0, 0, 0, 0, 0},
+				[]int{0, 0, 0, 0, 0, 0, 0},
+				[]int{0, 0, 0, 0, 0, 0, 0},
+				[]int{0, 0, 0, 0, 0, 0, 0},
+				[]int{0, 0, 0, 0, 0, 0, 0},
+				[]int{0, 0, 0, 0, 0, 0, 0},
+				[]int{0, 1, 1, 1, 1, 0, 0},
+				[]int{1, 0, 0, 0, 0, 1, 0},
+				[]int{1, 1, 1, 1, 1, 1, 0},
+				[]int{1, 0, 0, 0, 0, 0, 0},
+				[]int{1, 0, 0, 0, 0, 1, 0},
+				[]int{0, 1, 1, 1, 1, 0, 0},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		text := NewText(image.Point{0, 0}, test.toWrite, colornames.White)
+		img := image.NewRGBA(image.Rect(0, 0, 100, 100))
+		text.Draw(img)
+
+		for y, yi := range test.expected {
+			for x, xi := range yi {
+				actual := img.At(x, y)
+				shouldBeWhite := (xi == 1)
+				expected := color.RGBA{}
+				if shouldBeWhite {
+					expected = colornames.White
+				}
+				if actual != expected {
+					t.Errorf("for '%v' at {%v, %v}: expected %v, but got %v", test.toWrite, x, y, expected, actual)
+				}
+			}
+		}
+	}
+}
+
+func TestTextBounds(t *testing.T) {
 	tests := []struct {
 		toWrite  string
 		expected image.Rectangle
